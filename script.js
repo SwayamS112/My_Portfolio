86b2b3375bcdf233d2ac0beeca9ff95ceb72d5c3
Swayam Sood <148384397+SwayamS112@users.noreply.github.com>
2024-11-10 00:26:44 +0530

    added some animation

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-    window.addEventListener('scroll', function() {
-        var nav = document.querySelector('nav');
-        if (window.scrollY > 0) { 
-            nav.classList.add('scrolled');
+window.addEventListener('scroll', function() {
+    var nav = document.querySelector('nav');
+    if (window.scrollY > 0) { 
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const allSections = document.querySelectorAll('#All > div, #skills .skill, #projects .project-container-1, #projects .project-container-2');
+
+    const observerOptions = {
+        threshold: 0.4 
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show'); 
+            }
+        });
+    }, observerOptions);
+
+    allSections.forEach((section, index) => {
+        if (index % 2 === 0) {
+            section.classList.add('scroll-left');
         } else {
-            nav.classList.remove('scrolled');
+            section.classList.add('scroll-right');
         }
+        observer.observe(section);
     });
+});
