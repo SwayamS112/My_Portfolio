039cb960204f8fb03a38be907619e0b60a2a2394
Swayam Sood <148384397+SwayamS112@users.noreply.github.com>
2024-11-09 17:54:43 +0530

    full completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     window.addEventListener('scroll', function() {
         var nav = document.querySelector('nav');
-        if (window.scrollY > 0) { // When the user starts scrolling
+        if (window.scrollY > 0) { 
             nav.classList.add('scrolled');
         } else {
             nav.classList.remove('scrolled');
